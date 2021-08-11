cf88329279d153f1e31df54c62768313c203e53b
evgeniyryzhkin <jmaster.me@gmail.com>
2021-08-11 23:25:18 +0300

    update2

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# Press the green button in the gutter to run the script.
+
+@app.route('/', methods=["POST"])
+def root():
+    return 'Ok'
+
+
 if __name__ == '__main__':
     app.run(host='192.168.0.31', port=8080)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
