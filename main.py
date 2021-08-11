696f8a747c607ca7b25cab2edc3fd816b927a31f
evgeniyryzhkin <jmaster.me@gmail.com>
2021-08-11 23:16:48 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    app.run(host='192.168.0.31')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
