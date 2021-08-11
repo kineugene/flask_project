e9638e11f9009816e87ede613bb052bd4c4ad6ca
evgeniyryzhkin <jmaster.me@gmail.com>
2021-08-11 23:21:32 +0300

    update

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask, request
+from flask import Flask
 
 app = Flask(__name__)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    app.run(host='192.168.0.31')
+    app.run(host='192.168.0.31', port=8080)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
